21cd398216cbaedf1adad483afdb62e812d5240e
David Kowis <david.kowis@rackspace.com>
2012-06-12 13:41:34 -0500

    updating to the latest version where this works.

--- src/test/java/com/rackspace/example/stackTest.java
+++ src/test/java/com/rackspace/example/stackTest.java
@@ -1,11 +1,9 @@
 package com.rackspace.example;
 
 import cucumber.junit.Cucumber;
-import cucumber.junit.Cucumber.Options;
-import org.junit.Ignore;
 import org.junit.runner.RunWith;
 
 @RunWith(Cucumber.class)
-@Options(features = "RackStack.feature", format = "pretty")
+@Cucumber.Options(features = "classpath:", format = "pretty")
 public class stackTest {
 }
